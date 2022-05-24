ad55bce2c2fb39278b1db4d558287fba3cc29af3
Sergiu Manoliu <manoliusergiua@gmail.com>
2022-05-24 23:52:16 +0300

    added project

---
+++ WebApplication2/WebApplication2/Controllers/WeatherController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using WebApplication2.Models;
+using Newtonsoft.Json;
+
+namespace WebApplication2.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public String WeatherDetail(String city)
+        {
+            string key = "57f296c5b223ce3e5880dd155bcb38da";
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", city, key);
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                WeatherModel.RawData weatherInfo = JsonConvert.DeserializeObject<WeatherModel.RawData>(json);
+                WeatherModel.ResultViewModel rslt = new WeatherModel.ResultViewModel();
+
+                rslt.Country = weatherInfo.sys.country;
+                rslt.City = weatherInfo.name;
+                rslt.Lat = Convert.ToString(weatherInfo.coord.lat);
+                rslt.Lon = Convert.ToString(weatherInfo.coord.lon);
+                rslt.Description = weatherInfo.weather[0].description;
+                rslt.Humidity = Convert.ToString(weatherInfo.main.humidity);
+                rslt.Temp = Convert.ToString(weatherInfo.main.temp);
+                rslt.TempFeelsLike = Convert.ToString(weatherInfo.main.feels_like);
+                rslt.TempMax = Convert.ToString(weatherInfo.main.temp_max);
+                rslt.TempMin = Convert.ToString(weatherInfo.main.temp_min);
+                rslt.WeatherIcon = weatherInfo.weather[0].icon;
+
+                //Converting OBJECT to JSON String   
+                var jsonstring = JsonConvert.SerializeObject(rslt);
+
+                //Return JSON string.  
+                return jsonstring;
+            }
+        }
+        [HttpPost]
+        public String GetCountryImage( int x, int y, string countryCode)
+        {
+            return string.Format("https://flagcdn.com/{0}x{1}/{2}.png", x, y, countryCode.ToLower());
+        }
+
+    }
+}
---
+++ WebApplication2/WebApplication2/wwwroot/js/site.js
@@ -0,0 +1,53 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    //your code here
+
+    $("#btnSubmit").click(function () {
+
+        var cityname = $("#txtCity").val();
+        if (cityname.length > 0) {
+            $.ajax({
+                url: "https://localhost:44393/Weather/WeatherDetail?city=" + cityname,
+                type: "POST",
+                success: function (rsltval) {
+                    var data = JSON.parse(rsltval);
+                    console.log(data);
+                    $("#lblCity").html(data.City);
+                    $("#lblCountry").text(data.Country);
+                    $("#lblLat").text(data.Lat);
+                    $("#lblLon").text(data.Lon);
+                    $("#lblDescription").text(data.Description);
+                    $("#lblHumidity").text(data.Humidity);
+                    $("#lblTempFeelsLike").text(data.TempFeelsLike);
+                    $("#lblTemp").text(data.Temp);
+                    $("#lblTempMax").text(data.TempMax);
+                    $("#lblTempMin").text(data.TempMin);
+                    $("#imgWeatherIconUrl").attr("src", "http://openweathermap.org/img/w/" + data.WeatherIcon + ".png");
+                    var a = "";
+                    $.ajax({
+                        url: "https://localhost:44393/Weather/GetCountryImage?x=32&&y=24&&countryCode=" + data.Country.toLowerCase(),
+                        type: "POST",
+                        success: function (link) {
+                            a = link;
+                            $("#imgCountry").attr("src",a);
+                        },
+                        error: {
+
+                        }
+                    });
+
+                    $("#gmap_canvas").attr("src", "https://maps.google.com/maps?q=" + data.City+"&z=12&output=embed", )
+                },
+                error: function () {
+
+                }
+            });
+        }
+        else {
+            alert("City Not Found");
+        }
+    });
+});
